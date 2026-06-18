0d0d86eb643fc215dfbf984de69ef9a9fe9a00a1
Darshik924 <darshikladhe182@gmail.com>
2026-06-18 10:17:15 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -7,6 +7,11 @@ int main(int argc, char *argv[]) {
 
   // TODO: Uncomment the code below to pass the first stage
   printf("$ ");
+  char command[1024];
+  fgets(command, sizeof(command), stdin);
+
+  command[strcspn(command, "\n")] = '\0';
+  printf("%s: command not found", command);
 
   return 0;
 }
