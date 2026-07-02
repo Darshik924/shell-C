6df36427a9a8c8ffd1bccc7798d3e682af083ff2
Darshik924 <darshikladhe182@gmail.com>
2026-07-02 17:42:58 +0530

    Analogy

--- src/redir.c
+++ src/redir.c
@@ -69,8 +69,16 @@ void handleEcho(char *cmd)
         printf("%s\n$ ", st);
         return;
     }
+    // Linux has specific descriptor Tables for each process(like 0 stdin,1 stdout, 2 stderr)
+    // we will fool printf and change stdout descriptor table to that of file we want
+
+    // Our program has a fixed #1 slot labelled as screen
+    // int saved_fd = dup(1); // dup() will duplicate the "screen" number are write it to a new number and return that (say 5)
 
     int saved_fd = dup(isStdErr ? 2 : 1);
+    // so before doing anything we make a copy of contact info on Page 1 or 2 (if its an err need to be redirected to a file) and later on we will restore the copy of terminal output
+    // if we overwrite Page 1 and loose the "screen" no. then no way to get it back, this is just a save
+    // Now you have two ways to reach the screen og #1 slot and the saved_fd slot
     if (saved_fd < 0)
     {
         perror("dup");
@@ -87,11 +95,22 @@ void handleEcho(char *cmd)
     while (fnPtr[i] != '\0' && fnPtr[i] != ' ' && fnPtr[i] != '\n' && fnPtr[i] != '\t')
         fileName[i++] = fnPtr[i];
     fileName[i] = '\0';
+    // Extract the fileName
+
+    // So here we want to redirect the output from Standard Output 1 (basically the screen) to a file instead
+    // There is this phone Book analogy for linux descriptor tables. Imagine you have phone book and pages numbers for stuff
+    // 0 is stdin, 1 is stdout, 2 is stderr
+    // when we run printf, the program looks at 1 and see that text is to be printed to the screen
 
     int flags = O_WRONLY | O_CREAT | (isAppend ? O_APPEND : O_TRUNC);
     // Access modes are open write only, create if missing, append at the end file, truncate existing file to zero length
-    int fd = open(fileName, flags, 0644);
-    /* Open the file for reading or writing to the file */
+    int fd = open(fileName, flags, 0644); // this fd is the Page 6 (say)
+    /* The OS opens out.txt and gives you a new temporary number (say 6) */
+    // Nothing has been redirected yet; you just have the file ready to use.
+
+    // O_CREAT - create if does not exist
+    // O_TRUNC - if using > empty the file
+    // O_APPEND - if using >> make sure text is appended
     if (fd < 0)
     {
         // handle errors to the file
@@ -100,7 +119,7 @@ void handleEcho(char *cmd)
         return;
     }
 
-    if (dup2(fd, isStdErr ? 2 : 1) < 0)
+    if (dup2(fd, isStdErr ? 2 : 1) < 0) // This is the specific command that changes the direction. dup2(source, target) means: "Copy the number from source and force it into target, destroying whatever was there before."
     {
         // handle erros to the file
         perror("dup2 failed");
@@ -108,11 +127,18 @@ void handleEcho(char *cmd)
         close(saved_fd);
         return;
     }
+    /* The system looks at slot #6 (fd), which holds the "File" number.
+    It looks at slot #1 (the target). It sees "Screen" there.
+    It erases "Screen" from slot #1.
+    It writes "File" into slot #1  */
     close(fd);
-
+    // cleaning up the temporary numbers
+    
     printf("%s\n", st);
+    // Your program (and printf) doesn't know anything changed. It is hard-coded to always look at slot #1. It looks at #1, sees a number, and dials it. 
+    // It just happens to be the File's number now instead of the Screen's. The direction of data flow has physically changed because the definition of "Standard Output" (slot #1) has changed.
 
-    if (dup2(saved_fd, isStdErr ? 2 : 1) < 0)
+    if (dup2(saved_fd, isStdErr ? 2 : 1) < 0) // You are done writing to the file. If you end the program now, it’s fine. But if you want to keep running and print more things to the screen, you must fix slot #1
     {
         perror("dup2 restore");
         close(saved_fd);
@@ -120,6 +146,7 @@ void handleEcho(char *cmd)
     }
     close(saved_fd);
 
+    // The core secret is that printf never changes. It always dials "1". You are simply changing the contact info stored at address "1" temporarily.
     printf("$ ");
     fflush(stdout);
 }
